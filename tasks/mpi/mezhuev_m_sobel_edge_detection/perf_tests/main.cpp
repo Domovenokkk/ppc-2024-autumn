50fa653a2b4e04c7d1d5c362db5c21a87d0545c3
Domovenokkk <mezuevmaksim46@gmail.com>
2024-12-12 13:18:19 +0300

    Fix test

--- tasks/mpi/mezhuev_m_sobel_edge_detection/perf_tests/main.cpp
+++ tasks/mpi/mezhuev_m_sobel_edge_detection/perf_tests/main.cpp
@@ -6,35 +6,7 @@
 
 #include "mpi/mezhuev_m_sobel_edge_detection/include/ops_mpi.hpp"
 
-namespace mezhuev_m_sobel_edge_detection {
-
-TEST(GridTorusTopologyParallelPerfTests, PreProcessingPerformance) {
-  boost::mpi::environment env;
-  boost::mpi::communicator world;
-
-  size_t width = 1920;
-  size_t height = 1080;
-
-  mezhuev_m_sobel_edge_detection::TaskData task_data;
-  task_data.inputs.push_back(new uint8_t[width * height]());
-  task_data.inputs_count.push_back(width * height);
-  task_data.outputs.push_back(new uint8_t[width * height]());
-  task_data.outputs_count.push_back(width * height);
-
-  mezhuev_m_sobel_edge_detection::GridTorusTopologyParallel grid_topology(world);
-
-  auto start = std::chrono::high_resolution_clock::now();
-  ASSERT_TRUE(grid_topology.pre_processing());
-  auto end = std::chrono::high_resolution_clock::now();
-
-  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-  std::cout << "Pre-processing duration: " << duration.count() << " ms" << std::endl;
-
-  delete[] task_data.inputs[0];
-  delete[] task_data.outputs[0];
-}
-
-TEST(GridTorusTopologyParallelPerfTests, RunPerformance) {
+TEST(mezhuev_m_sobel_edge_detection, RunPerformance) {
   boost::mpi::environment env;
   boost::mpi::communicator world;
 
@@ -65,7 +37,35 @@ TEST(GridTorusTopologyParallelPerfTests, RunPerformance) {
   delete[] task_data.outputs[0];
 }
 
-TEST(GridTorusTopologyParallelPerfTests, ValidationPerformance) {
+namespace mezhuev_m_sobel_edge_detection {
+
+TEST(mezhuev_m_sobel_edge_detection, PreProcessingPerformance) {
+  boost::mpi::environment env;
+  boost::mpi::communicator world;
+
+  size_t width = 1920;
+  size_t height = 1080;
+
+  mezhuev_m_sobel_edge_detection::TaskData task_data;
+  task_data.inputs.push_back(new uint8_t[width * height]());
+  task_data.inputs_count.push_back(width * height);
+  task_data.outputs.push_back(new uint8_t[width * height]());
+  task_data.outputs_count.push_back(width * height);
+
+  mezhuev_m_sobel_edge_detection::GridTorusTopologyParallel grid_topology(world);
+
+  auto start = std::chrono::high_resolution_clock::now();
+  ASSERT_TRUE(grid_topology.pre_processing());
+  auto end = std::chrono::high_resolution_clock::now();
+
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  std::cout << "Pre-processing duration: " << duration.count() << " ms" << std::endl;
+
+  delete[] task_data.inputs[0];
+  delete[] task_data.outputs[0];
+}
+
+TEST(mezhuev_m_sobel_edge_detection, ValidationPerformance) {
   boost::mpi::environment env;
   boost::mpi::communicator world;
 
