539bfcbf6d45064f6a00b25d6c1d0a29f21dc3a3
Domovenokkk <mezuevmaksim46@gmail.com>
2024-12-21 20:08:14 +0300

    delete test

--- tasks/mpi/mezhuev_m_most_different_neighbor_elements/func_tests/main.cpp
+++ tasks/mpi/mezhuev_m_most_different_neighbor_elements/func_tests/main.cpp
@@ -271,31 +271,3 @@ TEST(mezhuev_m_most_different_neighbor_elements, RunWithNullPointersTest) {
   ASSERT_FALSE(task.validation());
   ASSERT_TRUE(task.pre_processing());
 }
-
-TEST(mezhuev_m_most_different_neighbor_elements, PostProcessingValidOutputTest) {
-  boost::mpi::environment env;
-  boost::mpi::communicator world;
-
-  const size_t data_size = 10;
-  std::vector<int> input_data(data_size);
-  std::iota(input_data.begin(), input_data.end(), 0);
-
-  auto taskData = std::make_shared<ppc::core::TaskData>();
-  taskData->inputs.resize(1);
-  taskData->inputs[0] = reinterpret_cast<uint8_t*>(input_data.data());
-  taskData->inputs_count.resize(1);
-  taskData->inputs_count[0] = data_size;
-
-  taskData->outputs.resize(2);
-  taskData->outputs[0] = reinterpret_cast<uint8_t*>(new int[1]{1});
-  taskData->outputs[1] = reinterpret_cast<uint8_t*>(new int[1]{0});
-  taskData->outputs_count.resize(1);
-  taskData->outputs_count[0] = 1;
-
-  mezhuev_m_most_different_neighbor_elements::MostDifferentNeighborElements task(world, taskData);
-
-  ASSERT_TRUE(task.validation());
-  ASSERT_TRUE(task.pre_processing());
-  ASSERT_TRUE(task.run());
-  ASSERT_FALSE(task.post_processing());
-}
